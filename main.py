9cfb8532f951361fb1c2e0330cc2f7eb16bf2c97
Ta52j <Tlal22336677@outlook.com>
2026-01-07 09:15:37 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+from pathlib import Path
+
+import darkdetect
+from PySide6.QtGui import QAction, QIcon
+from PySide6.QtWidgets import QApplication, QMenu, QSystemTrayIcon
+
+# Create QApplication
+app = QApplication([])
+app.setQuitOnLastWindowClosed(False)
+
+TRASH_PATH = str(Path.home() / ".local/share/Trash/files/")
+ICON_DARK_EMPTY = QIcon("icons/EmptyBinLight.png")
+ICON_DARK = QIcon("icons/BinLight.png")
+ICON_LIGHT_EMPTY = QIcon("icons/EmptyBinDark.png")
+ICON_LIGHT = QIcon("icons/BinDark.png")
+
+
+# Empty trash function
+def empty_trash():
+    for root_dir_path, dirs, files in os.walk(TRASH_PATH, True):
+        for file in files:
+            file_path = os.path.join(root_dir_path, file)
+            os.remove(file_path)
+        for dir in dirs:
+            dir_path = os.path.join(root_dir_path, dir)
+            os.rmdir(dir_path)
+
+
+# Create the menu
+systray_menu = QMenu()
+
+# Add a empty trash option
+systray_menu_empty = QAction("Empty")
+systray_menu_empty.triggered.connect(empty_trash)
+systray_menu.addAction(systray_menu_empty)
+
+# Add a quit option
+systray_menu_quit = QAction("Quit")
+systray_menu_quit.triggered.connect(app.quit)
+systray_menu.addAction(systray_menu_quit)
+
+# Create the system tray
+systray = QSystemTrayIcon()
+if darkdetect.theme() == "light":
+    systray.setIcon(ICON_LIGHT_EMPTY)
+else:
+    systray.setIcon(ICON_DARK_EMPTY)
+systray.setVisible(True)
+systray.setContextMenu(systray_menu)
+
+# Run the app
+app.exec()
